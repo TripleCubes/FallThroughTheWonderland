44c88eff2e23cc57760a3c245f01663ea803eb25
TripleCubes <113813506+TripleCubes@users.noreply.github.com>
2023-08-21 09:51:48 +0700

    Stats and coin result wont be shown at game start

--- Fairy/Scripts/Singletons/global_vars.gd
+++ Fairy/Scripts/Singletons/global_vars.gd
@@ -1,6 +1,8 @@
 extends Node
 
 @onready var _map: TileMap = get_node(Consts.MAIN_PATH + "TileMap")
+@onready var _stats = get_node(Consts.UI_PATH + "Stats")
+@onready var _coin_result = get_node(Consts.UI_PATH + "CoinResult")
 
 var game_started: = false:
 	set(val):
@@ -8,6 +10,12 @@ var game_started: = false:
 
 		if game_started:
 			_open_hole()
+			var tween_0: = get_tree().create_tween()
+			tween_0.tween_property(_stats, "position", Vector2(4, _stats.position.y), 
+										1.2).set_trans(Tween.TRANS_SINE)
+			var tween_1: = get_tree().create_tween()
+			tween_1.tween_property(_coin_result, "position", Vector2(0, _coin_result.position.y), 
+										1.2).set_trans(Tween.TRANS_SINE)
 		else:
 			GlobalFunctions.restart_game()
 
